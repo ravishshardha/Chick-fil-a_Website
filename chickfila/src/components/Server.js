ee39ffe30adc371680cf8148fa562cc7e1d176d7
Lydia-Harding <lydiacharding@tamu.edu>
2023-04-05 13:58:28 -0500

    making server branch, starting server functionality

--- chickfila/src/components/Server.js
+++ chickfila/src/components/Server.js
@@ -1,6 +1,70 @@
 import React from 'react'
 import '../css/Server.css'
+import chickfilalogo from '../resources/chickfilalogo.png'
 
+// hardcoded for now
+const menuitems = [
+    {
+        id: 1,
+        text: "Chicken Sandwich",
+        price: 5.99,
+    },
+    {
+        id: 2,
+        text: "Spicy Chicken sandwich",
+        price: 6.99,
+    },
+    {
+        id: 3,
+        text: "Side salad",
+        price: 3.99,
+    }
+]
+
+// this is our order information
+let order = []
+
+function OrderSummary(){
+    const listItems = menuitems.map(item =>
+        <li key= {item.id}>
+            <p>{item.name}......{item.price}</p>
+        </li>
+    )
+    return(
+        <div className='ServerOrderSummary'>
+            <ul>{listItems}</ul>
+        </div>
+    );
+}
+
+function ServerButton(){
+    const listItems = menuitems.map(item =>
+        <button className='ServerButton'
+           // onClick={}
+        >
+                {item.text} ${item.price}
+        </button>
+    )
+    return (
+        <div className='ServerButtonBar'>
+            {listItems}
+        </div>
+    );
+}
+
+function ScrollingButtons(){
+    return (
+        <div className='ServerScrollingButtons'>
+            <p>TODO, make buttons capable of being dynamically allocated</p>
+            <div>
+                <ServerButton
+                    text={'Testbutton'}
+                    price={5.99}
+                />
+            </div>
+        </div>
+    );
+}
 
 export default function Server(){
     return(
@@ -14,8 +78,10 @@ export default function Server(){
             <p>Type type type type</p>
             <p>Type type type type</p>
             <p>Finally there are enough rows so you can see the scroll.</p>
-
-
+            <div>Order Summary:</div>
+            <OrderSummary/>
+            <ScrollingButtons/>
+            
         </div>
     );
 }
