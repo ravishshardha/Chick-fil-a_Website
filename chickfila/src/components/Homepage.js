8ae8978a1b26b7fd97431ab246a2323f5f9e162c
Lydia-Harding <lydiacharding@tamu.edu>
2023-04-05 15:46:53 -0500

    adding a homepage

---
+++ chickfila/src/components/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage(){
+    return(
+        <div className='Homepage'>
+            Here we will have links to the other pages
+        </div>
+    );
+}
