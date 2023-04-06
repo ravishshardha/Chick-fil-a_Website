51cdcd4dc0eaaaa3e2f215610e8a2ac10c1edbcf
Lydia-Harding <lydiacharding@tamu.edu>
2023-04-06 09:56:14 -0500

    added a generalized table component, modified manager to show an example

--- chickfila/src/components/Manager.js
+++ chickfila/src/components/Manager.js
@@ -1,5 +1,79 @@
 import React from 'react';
 import '../css/Manager.css'
+import Table from './GeneralTable'
+
+const orderData = [
+    {
+        orderid: 1,
+        time: "Monday",
+        price: 20.0,
+        star_sign: "Taurus",
+    },
+    {
+        orderid: 2,
+        time: "Tuesday",
+        price: 30.0,
+        star_sign: "Cancer",
+    },
+    {
+        orderid: 3,
+        time: "Wednesday",
+        price: 40.0,
+        star_sign: "Pickles"
+    },
+    {
+        orderid: 3,
+        time: "Wednesday",
+        price: 40.0,
+        star_sign: "Virgo",
+    },
+]
+
+const menuItems = [
+    {
+        id: 1,
+        name: "Chicken Sandwich",
+        price: 5.99,
+    },
+    {
+        id: 2,
+        name: "Spicy Chicken Sandwich",
+        price: 7.99,
+    },
+    {
+        id: 3,
+        name: "Chicken Nuggets (5)",
+        price: 4.99,
+    },
+    {
+        id: 4,
+        name: "Side Salad",
+        price: 2.99,
+    },
+]
+
+const inventory = [
+    {
+        id: 1,
+        name: "Chicken patty",
+        stock: 50,
+    },
+    {
+        id: 2,
+        name: "Buns",
+        stock: 70,
+    },
+    {
+        id: 3,
+        name: "Nugget",
+        stock: 40,
+    },
+    {
+        id: 4,
+        name: "Lettuce",
+        stock: 80,
+    },
+]
 
 function Orders(){
     return(
@@ -13,6 +87,9 @@ function Orders(){
                 <p>[TODO: fill with information about orders]</p>
                 <p>[order table]</p>
                 <p>[Needs a table, and this table will be filled with orders.]</p>
+                <div className='scrollingTable'>
+                    <Table data={orderData}/>
+                </div>
             </div>
             <hr></hr>
         </div> 
@@ -29,6 +106,9 @@ function Menu(){
             <div className='managerMenu viewTable'>
                 <p>[TODO fill with table of menu items, similar to menu view, but populated with more information]</p>
                 <p>[Needs a table of menu items.]</p>
+                <div className='scrollingTable'>
+                    <Table data={menuItems}/>
+                </div>
             </div>
             <hr></hr>
         </div>
@@ -45,6 +125,9 @@ function Inventory(){
             <div className='managerInventory viewTable'>
                 <p>[TODO fill with a table of ingredient information]</p>
                 <p>[Needs a table of menu items.]</p>
+                <div className='scrollingTable'>
+                    <Table data={inventory}/>
+                </div>
             </div>
             <hr></hr>
         </div>
