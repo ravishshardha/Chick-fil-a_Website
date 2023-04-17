7ac0271fd82657544ba9106e802c3a878b6c35c2
unknown <dcaoo@tamu.edu>
2023-04-17 12:26:48 -0500

    adding seasonal item

--- chickfila/backend/backend/routes/index.js
+++ chickfila/backend/backend/routes/index.js
@@ -49,35 +49,6 @@ app.get('/api/menu', (req, res) => {
 });
 
 
-app.get('/api/menu1', (req, res) => {
-  client.query('SELECT * FROM menu', (error, results) => {
-    if (error) {
-      console.log("unable to connect");
-      throw error;
-    }
-    console.log("sent");
-    res.json(results.rows);
-  });
-});
-
-// app.get('/api/postOrder', async (req, res) => {
-
-//   const result = client.query('SELECT orderid FROM orderslog1 ORDER BY orderid DESC LIMIT 1', [req.params.id]);
-
-//   const { time, employeeid, orderid,itemlist,price } = req.body;
-//   const now = new Date();
-//   time = now.toLocaleString('en-US', { hour12: false });
-//   console.log(time);
-//   res.status(201).json({ message: 'User created successfully' });
-//   // try {
-//   //   const result =  client.query('INSERT INTO data (time, employeeid, orderid,itemlist,price) VALUES ($1, $2, $3,$4, $5)', [time, employeeid, orderid,itemlist,price]);
-//   //   res.status(200).json(result.rows[0]);
-//   // } catch (error) {
-//   //   console.error(error);
-//   //   res.status(500).json({ error: 'Something went wrong' });
-//   // }
-// });
-
 app.get('/api/addOrder', (req, res) => {
   // create time
   const now = new Date();
@@ -164,6 +135,36 @@ arr.forEach((pair) => {
 }); 
   return map;
 }
+
+app.get('/api/addItem', (req, res) => {
+  // find id
+  client.query('SELECT id FROM menu ORDER BY id DESC LIMIT 1', (error, results) => {
+    if (error) {
+      console.log("unable to connect");
+      throw error;
+    }
+    const myValueString = results.rows[0].id;
+    const nextOrderId = parseInt(myValueString, 10) + 1; 
+    console.log(nextOrderId);
+    //client.query('INSERT INTO Menu (id,name ,price ,type, ingredients,url) VALUES ($1, $2, $3,$4, $5)', [id,itemName ,price ,type, ingredients,url]);
+  });
+});
+
+
+// seaonsal item*
+app.get('/api/addItem', (req, res) => {
+  // find id
+  client.query('SELECT id FROM menu ORDER BY id DESC LIMIT 1', (error, results) => {
+    if (error) {
+      console.log("unable to connect");
+      throw error;
+    }
+    const myValueString = results.rows[0].id;
+    const nextOrderId = parseInt(myValueString, 10) + 1; 
+    console.log(nextOrderId);
+    //client.query('INSERT INTO Menu (id,name ,price ,type, ingredients,url) VALUES ($1, $2, $3,$4, $5)', [id,itemName ,price ,type, ingredients,url]);
+  });
+});
 
 app.listen(5000, () => {
   console.log('Server started on port 5000');
