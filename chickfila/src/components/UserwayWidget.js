87ad0caf3a5b9b0248ca9c3b62378d8fab41a74a
ravishshardhaHere <ravishshardha@tamu.edu>
2023-04-28 22:18:17 -0500

    sizing fixed

--- chickfila/src/components/UserwayWidget.js
+++ chickfila/src/components/UserwayWidget.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 const UserwayWidget = () => {
   useEffect(() => {
     const script = document.createElement('script');
+    script.setAttribute("data-position", "10");
     script.setAttribute('data-account', 'G8Q4kJUUFu');
     script.setAttribute('src', 'https://cdn.userway.org/widget.js');
     document.body.appendChild(script);
